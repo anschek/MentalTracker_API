4157b96a5b0560b9bb15058c0e342af0cb6f69d7
anschek <anschek@yandex.ru>
2024-10-13 14:43:08 +0300

    feat: add DateOnlyConverter and registration of json options

---
+++ MentalTracker_API/Helpers/DateOnlyConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MentalTracker_API.Helpers
+{
+    public class DateOnlyConverter : JsonConverter<DateOnly>
+    {
+        private readonly string _format = "yyyy-MM-dd";
+        public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                var value = reader.GetString();
+                if (DateOnly.TryParseExact(value, _format, null, System.Globalization.DateTimeStyles.None, out var date)) return date;
+            }
+            throw new JsonException("Неверный формат даты.");
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options) =>        
+            writer.WriteStringValue(value.ToString(_format));
+        
+    }
+}
--- MentalTracker_API/Program.cs
+++ MentalTracker_API/Program.cs
@@ -1,5 +1,7 @@
+using MentalTracker_API.Helpers;
 using MentalTracker_API.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace MentalTracker_API
 {
@@ -10,9 +12,16 @@ namespace MentalTracker_API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllers();
-            builder.Services.AddDbContext<MentalTrackerContext>(options =>
-                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.Converters.Add(new DateOnlyConverter());
+                    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+                }); 
+            builder.Services
+                .AddDbContext<MentalTrackerContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+                );
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             var app = builder.Build();
